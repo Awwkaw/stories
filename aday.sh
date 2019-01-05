c159ddb6bbd4401ca19fa4fe1cadcf69000a8977
awwkaw <awwkaw@gmail.com>
2019-01-05 13:36:33 +0100

    aday.sh

--- aday.sh
+++ aday.sh
@@ -7,22 +7,30 @@ mkdir ./$DAY/english
 DFILE="./$DAY/dansk/story.tex"
 touch "$DFILE"
 echo "\\documentclass[oneside,danish,onecolumn,openbib,a4paper]{memoir}
-\\includepackage[utf8]{inputenc}
-\\includepackage[T1]{fontenc}
-\\includepackage{siunitx,mathtools}
-
+\\usepackage[utf8]{inputenc}
+\\usepackage[T1]{fontenc}
+\\usepackage{siunitx,mathtools}
+\\usepackage{hyperref}
+\\hypersetup{colorlinks=true}
 \\begin{document}
+Inspiered by:
+\url{<+The link to the writing prompt+>}
+
 
 \\end{document}" >> "$DFILE"
 
 EFILE="./$DAY/english/story.tex"
 touch "$EFILE"
 echo "\\documentclass[oneside,danish,onecolumn,openbib,a4paper]{memoir}
-\\includepackage[utf8]{inputenc}
-\\includepackage[T1]{fontenc}
-\\includepackage{siunitx,mathtools}
-
+\\usepackage[utf8]{inputenc}
+\\usepackage[T1]{fontenc}
+\\usepackage{siunitx,mathtools}
+\\usepackage{hyperref}
+\\hypersetup{colorlinks=true}
 \\begin{document}
+Inspiered by:
+\url{<+The link to the writing prompt+>}
+
 
 \\end{document}" >> "$EFILE"
 
